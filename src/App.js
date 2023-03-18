2c6ec0116b9e07d33223d0de5ee3c2e0287ae734
troymeeker <troymeeker1@gmail.com>
2023-03-17 18:31:05 -0700

    search built but not working

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import './App.css';
 import Search from './Components/Search';
 
 function App() {
+
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  }
   return (
     <div className='container'>
-       <Search />
+       <Search onSearchChange={handleOnSearchChange}/>
     </div>
  
   );
