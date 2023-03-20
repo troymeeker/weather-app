3f6df989407a94130acf06e6860ae65d5c6ef457
troymeeker <troymeeker1@gmail.com>
2023-03-19 20:46:24 -0700

    fetching but has errors

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,9 +1,18 @@
 import { useState } from "react"
-import { AsyncPaginate } from "react-select-async-paginate"
+import { AsyncPaginate } from "react-select-async-paginate";
+import { API_URL, APIOptions } from "../api"
 
 const Search = ({onSearchChange}) => { 
 
     const [search, setSearch ] = useState(null)
+
+    const loadOptions = (inputValue) => {
+       return fetch(`${API_URL}/adminDivions?minPopulation=9999&namePrefix=${inputValue}`, APIOptions)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
+
+    } 
 
     const handleOnChange = (searchData) => {
         setSearch(searchData);
@@ -15,6 +24,7 @@ const Search = ({onSearchChange}) => {
     debounceTimeout={600}
     value={search}
     onChange={handleOnChange}
+    loadOptions={loadOptions}
     />) 
 }
 
---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const APIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c34e0794e5mshd9994e6efce566ep11050fjsn98181178ac2e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
