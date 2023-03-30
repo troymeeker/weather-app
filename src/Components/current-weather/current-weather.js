1948a66b6c97dc23bb7b6cd7515ad36403ba1884
troymeeker <troymeeker1@gmail.com>
2023-03-29 21:20:28 -0700

    basic styling added for weather box

--- src/Components/current-weather/current-weather.js
+++ src/Components/current-weather/current-weather.js
@@ -1,17 +1,19 @@
-import "./current-weather.css"
+import "./current-weather.css";
 
 const CurrentWeather = () => {
-    return (
-        <div className="weather">
-            <div className="top">
-                <p className="city">City Name </p>
-                <p className="curr-weather-desc">Weather Description</p>
-            
-            
-            </div>
-            <img alt="weather" className="weather-icon" src="icons/01d.png"/>
+  return (
+    <div className="weather">
+      <div className="top">
+        
+        <img alt="weather" className="weather-icon" src="icons/01d.png" />
+        <div>
+          <p className="city">City Name </p>
+          <p className="curr-weather-desc">Weather Description </p>
         </div>
-    );
-}
+      </div>
+   
+    </div>
+  );
+};
 
 export default CurrentWeather;
