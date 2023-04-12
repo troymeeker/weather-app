79cf2d720984e8e080830010c94084a4ed4418b5
troymeeker <troymeeker1@gmail.com>
2023-04-11 21:51:41 -0700

    details added, convert added but not functioning totally

--- src/Components/current-weather/current-weather.js
+++ src/Components/current-weather/current-weather.js
@@ -1,6 +1,13 @@
 import "./current-weather.css";
+import { useState } from "react";
 
 const CurrentWeather = () => {
+
+  const [state, setState] = useState('Fahrenheit')
+
+  function handleConvert({state}){
+    setState('Celsius')
+  }
   return (
     <div className="weather">
       <div className="top">
@@ -10,6 +17,20 @@ const CurrentWeather = () => {
           <p className="city">City Name </p>
           <p className="curr-weather-desc">Weather Description </p>
         </div>
+      </div>
+      <div className="bottom">
+        <p className="temp-f">32°F</p>
+        {/* <p className="temp-c">0°C</p> */}
+        <div className="details">
+          <div className="param-row">
+            <div className="param-label">Details</div>
+          </div>
+          <div className="param-row">
+            <div className="param-label">Feels like:</div>
+            <div className="param-value">30°F</div>
+          </div>
+        </div>
+        <button className="convert" onClick={handleConvert}>Convert to {state}</button>
       </div>
    
     </div>
